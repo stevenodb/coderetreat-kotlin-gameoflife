b61058325b2313a209b753678f2ae7b50eced412
Steven Op de beeck <steven@opdebeeck.org>
2023-02-25 14:26:25 +0100

    cleanup

--- src/main/kotlin/be/swsb/coderetreat/Universe.kt
+++ src/main/kotlin/be/swsb/coderetreat/Universe.kt
@@ -1,4 +1,10 @@
 package be.swsb.coderetreat
 
-class Universe(val livingCells: List<Pair<Int, Int>>) {
+class Universe(livingPairs: List<Pair<Int, Int>>) {
+    val livingCells: MutableMap<Pair<Int, Int>, Cell> = mutableMapOf()
+
+    init {
+        livingPairs.forEach { pair -> livingCells[pair] = Cell(true) }
+    }
+
 }
