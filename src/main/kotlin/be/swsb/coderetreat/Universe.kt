98c885555388c1867dce22823646f061e452bde9
Steven Op de beeck <steven@opdebeeck.org>
2023-02-25 21:36:50 +0100

    new version

--- src/main/kotlin/be/swsb/coderetreat/Universe.kt
+++ src/main/kotlin/be/swsb/coderetreat/Universe.kt
@@ -1,10 +1,29 @@
 package be.swsb.coderetreat
 
-class Universe(livingPairs: List<Pair<Int, Int>>) {
-    val livingCells: MutableMap<Pair<Int, Int>, Cell> = mutableMapOf()
+class Universe(val livingCells: List<Cell>) {
 
-    init {
-        livingPairs.forEach { pair -> livingCells[pair] = Cell(true) }
+    fun tick(): Universe {
+        val newLivingCells = livingCells.mapNotNull { cell ->
+            when (livingNeighbourCount(cell)) {
+                in 0..1 -> null
+                in 4..8 -> null
+                else -> cell
+            }
+        }.toList()
+        val deadCells = livingCells.flatMap { neighbouringCells(it) } - livingCells.toSet()
+        val birthedCells = deadCells.filter { livingNeighbourCount(it) == 3 }.toSet()
+        return Universe(newLivingCells + birthedCells)
     }
 
+    private fun neighbouringCells(cell: Cell): List<Cell> {
+        val neighbouringPairs = (-1..1).flatMap { ny -> (-1..1).map { nx -> Cell(nx, ny) } } - (Cell(0, 0))
+        return neighbouringPairs.map { cell + it }
+    }
+
+    private fun livingNeighbourCount(currentCell: Cell): Int =
+        neighbouringCells(currentCell).count { it in livingCells }
+}
+
+data class Cell(val x: Int, val y: Int) {
+    operator fun plus(other: Cell): Cell = Cell(this.x + other.x, this.y + other.y)
 }
