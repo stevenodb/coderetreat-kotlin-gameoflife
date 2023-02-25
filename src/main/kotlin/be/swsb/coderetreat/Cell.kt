315bf2bad6ce7bcfebd9a44b50a944656f2c52bb
Steven Op de beeck <steven@opdebeeck.org>
2023-02-25 13:51:36 +0100

    Cell with 3 neighbours stays alive

--- src/main/kotlin/be/swsb/coderetreat/Cell.kt
+++ src/main/kotlin/be/swsb/coderetreat/Cell.kt
@@ -1,4 +1,7 @@
 package be.swsb.coderetreat
 
 data class Cell(val isAlive: Boolean) {
+    fun tick(livingNeighbours: Int): Cell {
+        return this
+    }
 }
--- src/test/kotlin/be/swsb/coderetreat/CellTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/CellTest.kt
@@ -1,12 +1,20 @@
 package be.swsb.coderetreat
 
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class CellTest {
     @Test
     fun `Cell can be alive`() {
         val cell = Cell(true)
-        Assertions.assertThat(cell.isAlive).isTrue
+        assertThat(cell.isAlive).isTrue
+    }
+
+    @Test
+    fun `Living cell with 3 nb stays avlive`() {
+        val cell = Cell(true).tick(3)
+        assertThat(cell.isAlive).isTrue()
     }
 }
