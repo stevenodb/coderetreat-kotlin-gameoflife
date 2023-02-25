e0733911a23abad2ad28ce6a6a0b77e85bcb785d
Steven Op de beeck <steven@opdebeeck.org>
2023-02-25 14:02:06 +0100

    Refactor tick to use when statement

--- src/main/kotlin/be/swsb/coderetreat/Cell.kt
+++ src/main/kotlin/be/swsb/coderetreat/Cell.kt
@@ -2,7 +2,12 @@ package be.swsb.coderetreat
 
 data class Cell(val isAlive: Boolean) {
     fun tick(livingNeighbours: Int): Cell {
-        return if (livingNeighbours > 3 || livingNeighbours < 2) Cell(false) else Cell(true)
-//        when (livingNeighbours) {}
+        return when (livingNeighbours) {
+            in 0..1 -> Cell(false)
+            in 4..8 -> Cell(false)
+            else -> {
+                Cell(true)
+            }
+        }
     }
 }
