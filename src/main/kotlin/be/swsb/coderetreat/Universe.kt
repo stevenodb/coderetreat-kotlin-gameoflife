d3b9c5fb47935ef4815d386f1e37dcca25105d75
Steven Op de beeck <steven@opdebeeck.org>
2023-02-26 11:35:33 +0100

    code cleanup

--- src/main/kotlin/be/swsb/coderetreat/Universe.kt
+++ src/main/kotlin/be/swsb/coderetreat/Universe.kt
@@ -1,30 +1,37 @@
 package be.swsb.coderetreat
 
-class Universe(val livingCells: Set<Cell>) {
-    constructor(vararg livingCell:Cell): this(livingCell.toSet())
+class Universe(val livingCellPositions: Set<CellPosition>) {
+    constructor(vararg livingCellPositions: CellPosition) : this(setOf(*livingCellPositions))
 
     fun tick(): Universe {
-        val newLivingCells = livingCells.filter { cell ->
+        val newLivingCells = livingCellPositions.filter { cell ->
             when (livingNeighbourCellCount(cell)) {
                 in 0..1 -> false
                 in 4..8 -> false
                 else -> true
             }
         }.toSet()
-        val deadCells = livingCells.flatMap { neighbouringCells(it) } - livingCells
+        val deadCells = livingCellPositions.flatMap { it.neighbouringCells() } - livingCellPositions
         val birthedCells = deadCells.filter { livingNeighbourCellCount(it) == 3 }.toSet()
         return Universe(newLivingCells + birthedCells)
     }
 
-    private fun neighbouringCells(cell: Cell): List<Cell> {
-        val neighbouringPairs = (-1..1).flatMap { ny -> (-1..1).map { nx -> Cell(nx, ny) } } - (Cell(0, 0))
-        return neighbouringPairs.map { cell + it }
-    }
-
-    private fun livingNeighbourCellCount(currentCell: Cell): Int =
-        neighbouringCells(currentCell).count { it in livingCells }
+    private fun livingNeighbourCellCount(cellPosition: CellPosition): Int =
+        cellPosition.neighbouringCells { it in livingCellPositions }.count()
 }
 
-data class Cell(val x: Int, val y: Int) {
-    operator fun plus(other: Cell): Cell = Cell(this.x + other.x, this.y + other.y)
+data class CellPosition(val x: Int, val y: Int) {
+
+    fun neighbouringCells(filterFunction: (CellPosition) -> Boolean = { true }): List<CellPosition> {
+        val allNeighbouringPositions =
+            (-1..1)
+                .flatMap { ny -> (-1..1)
+                    .filterNot { nx -> nx == 0 && ny == 0 }
+                    .map { nx -> CellPosition(nx, ny) } }
+        return allNeighbouringPositions
+            .map { this + it }
+            .filter(filterFunction)
+    }
+
+    private operator fun plus(other: CellPosition) = CellPosition(x + other.x, y + other.y)
 }
